8c19cfb995c2a8cef20bc1f1b737915e32f6b994
nlampetis <nikolastessera@hotmail.com>
2022-04-11 13:04:23 +0300

    created the class templates for the bouncy ball

---
+++ bouncingball.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+#include "console.h"
+
+struct coord {
+        float x;
+        float y;
+};
+
+
+class Bouncy{
+private:
+    coord pos;
+    coord vel;
+    CHAR_INFO chInfo;
+public:
+    
+    Bouncy();
+    virtual ~Bouncy();
+
+
+    const coord& getVel() const;
+    const coord& getPos() const;
+    void setVel(int& newx, int& newy);
+    void setPos(int& newx, int& newy);
+    const CHAR_INFO& getBouncyCharInfo() const;
+    
+    void move(const char&);
+    void draw(Console&);
+
+};
