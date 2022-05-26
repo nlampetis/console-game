723d884030934b681f6083c19db2f0e5e3ef8591
nlampetis <nikolastessera@hotmail.com>
2022-05-26 15:02:57 +0300

    sync

--- player.h
+++ player.h
@@ -9,45 +9,39 @@
 #include "console.h"
 #include "drawable.h"
 
+class Player : public Drawable {
+private:
+  int x;
+  int y;
+  CHAR_INFO *chInfo;
+  float internalTimeDelta;
 
-class Player : public Drawable{
-    private:
-        int x;
-        int y;
-        CHAR_INFO * chInfo;
-        float internalTimeDelta;
+public:
+  Player();
+  Player(int, int);
+  virtual ~Player();
 
-    public:
-        
-        Player();
-        Player(int, int);
-        virtual ~Player();
+  const int &getX() const;
+  const int &getY() const;
 
-        const int& getX() const;
-        const int& getY() const;
-        
-        void setX(int& newx);
-        void setY(int& newy);
+  void setX(int &newx);
+  void setY(int &newy);
 
-        void setPos(COORD newpos);
+  void setPos(COORD newpos);
 
-        //WCHAR getChar() const;
-        //WORD GetAttr() const;
+  // WCHAR getChar() const;
+  // WORD GetAttr() const;
 
-        
-        
-        COORD getPos() const;
-        short getWidth() const;
-        short getHeight() const;
-        CHAR_INFO * getSprite() const;
+  COORD getPos() const;
+  short getWidth() const;
+  short getHeight() const;
+  CHAR_INFO *getSprite() const;
 
-        //const CHAR_INFO& getPlayerCharInfo() const;
-        
-        void move(const char&);
-        //void draw(Console&);
+  // const CHAR_INFO& getPlayerCharInfo() const;
 
+  void move(const char &);
+  // void draw(Console&);
 
-        void incrementDelta(float toAddelta);
-        void automove1s();
-
+  void incrementDelta(float toAddelta);
+  void automove1s();
 };
